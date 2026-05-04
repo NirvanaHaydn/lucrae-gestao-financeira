f1509e4001e53f8aa510e74a10bdda9ad4cf50a7
Nirvana Oliveira <facnirv@gmail.com>
2026-05-04 19:35:25 -0300

    navbar js, primario

---
+++ codigo-fonte/js/components/navbar.js
@@ -0,0 +1,14 @@
+document.getElementById("navbar").innerHTML = `
+<nav class="navbar navbar-custom">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Lucraê</a>
+        <div class="d-flex">
+            <a href="index.html" class="nav-link me-3">Home</a>
+            <a href="about.html" class="nav-link me-3">Sobre Nós</a>
+            <a href="news.html" class="nav-link me-3">Notícias</a>
+            <a href="education.html" class="nav-link">Educação</a>
+        </div>
+    </div>
+</nav>
+`;
