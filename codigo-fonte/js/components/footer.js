f7b1343ca00d054fbc78633547c027a36b82e056
João Almeida <joaoalmeida0913@outlook.com>
2026-05-05 18:36:39 -0300

    Criado: Assest(Pasta) e img(Pasta) | Criado arquivo footer.js dentro da pasta components dentro da pasta js

---
+++ codigo-fonte/js/components/footer.js
@@ -0,0 +1,7 @@
+document.getElementById("footer").innerHTML = `
+<footer class="footer-custom">
+    <div class="container">
+        <p>© 2026 Lucraê | Grupo 2 - Todos os direitos reservados</p>
+    </div>
+</footer>
+`;
--- codigo-fonte/js/components/navbar.js
+++ codigo-fonte/js/components/navbar.js
@@ -14,10 +14,3 @@ document.getElementById("navbar").innerHTML = `
     </div>
 </nav>
 `;
-document.getElementById("footer").innerHTML = `
-<footer class="footer-custom">
-    <div class="container">
-        <p>© 2026 Lucraê | Grupo 2 - Todos os direitos reservados</p>
-    </div>
-</footer>
-`;
