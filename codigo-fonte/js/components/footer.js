0b919ae6b817e0b40e76b41ad14231741f0e6c3e
João Almeida <joaoalmeida0913@outlook.com>
2026-05-05 19:35:27 -0300

    Mais umas alterações aí

--- codigo-fonte/js/components/footer.js
+++ codigo-fonte/js/components/footer.js
@@ -1,7 +1,7 @@
 document.getElementById("footer").innerHTML = `
-<footer class="footer-custom">
+<footer class="footer-custom fixed-bottom">
     <div class="container">
-        <p>© 2026 Lucraê | Grupo 2 - Todos os direitos reservados</p>
+        <h3>© 2026 Lucraê | Grupo 2 - Todos os direitos reservados</h3>
     </div>
 </footer>
 `;
--- codigo-fonte/js/components/navbar.js
+++ codigo-fonte/js/components/navbar.js
@@ -1,15 +1,14 @@
 document.getElementById("navbar").innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-custom">
     <div class="container">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="index.html">
     <img src="../documentos/imguserflow/logo_nova.png" alt="Lucraê" class="logo">
 </a>
 
         <div class="d-flex">
-            <a href="index.html" class="nav-link me-3">Home</a>
-            <a href="about.html" class="nav-link me-3">Sobre Nós</a>
-            <a href="news.html" class="nav-link me-3">Notícias</a>
-            <a href="education.html" class="nav-link">Educação</a>
+            <a href="about.html" class="nav-link me-3"><button class="btn btn-outline-success me-1" type="button">Sobre nós</button></a>
+            <a href="news.html" class="nav-link me-3"><button class="btn btn-outline-success me-1" type="button">Notícias</button></a>
+            <a href="education.html" class="nav-link me-3"><button class="btn btn-outline-success me-2" type="button">Educação</button></a>
         </div>
     </div>
 </nav>
